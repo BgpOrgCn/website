4dbfb3d709bad7de87e0d66351c943fc54738503
chun_qiu <herodain@qq.com>
2024-10-06 23:42:41 +0800

    增加了.gitignore文件

--- node_modules/@algolia/client-common/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './cache';
-export * from './createClient';
-export * from './createIterablePromise';
-export * from './host';
-export * from './logger';
-export * from './requester';
-export * from './transporter';
